c344ae8416cf3373541f8e925f2c006f41e0711c
cyrusthelittle <60959846+cyrusthelittle@users.noreply.github.com>
2020-07-03 19:05:04 +0430

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,25 @@
+
+
+#first we need to install google translate library by 
+#pip install googletrans
+import googletrans
+from googletrans import Translator
+
+translator = Translator()
+print('Enter your text...')
+sourceText=input()
+a=translator.detect(sourceText)
+if a.lang == 'fa':
+    print('متن فارسی است')
+    destText=translator.translate(sourceText, dest='en')
+if a.lang == 'en':
+    print('text is English')
+    destText=translator.translate(sourceText, dest='fa')
+
+print(destText.text)
+
+
+
+
+
+
